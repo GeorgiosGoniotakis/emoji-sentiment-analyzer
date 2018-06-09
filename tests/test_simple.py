0230adbebe4608f60f896ec2cee0106c6c7fa6b5
Georgios Goniotakis <georgios.goniotakis@outlook.com>
2018-06-10 00:39:09 +0300

    Updated to enable coverage reports

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,9 +1,16 @@
 import unittest
 
+from source.data.EmojiLoader import EmojiLoader
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
+
+    def test_loader(self):
+        loader = EmojiLoader(None)
+        data = loader.load()
+        self.assertNotEqual(None, data)
 
 
 if __name__ == '__main__':
